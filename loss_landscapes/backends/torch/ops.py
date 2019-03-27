d1cc57d5c05cb878b5ffd144274a65afac344722
marcellodebernardi <marcello.logins@outlook.com>
2019-03-27 21:43:21 +0000

    Implement bad input checking for torch.ops

--- loss_landscapes/backends/torch/ops.py
+++ loss_landscapes/backends/torch/ops.py
@@ -1,4 +1,8 @@
-""" Defines basic linear algebra operations on lists of torch.nn.Parameter objects. """
+"""
+Defines basic linear algebra operations on lists of torch.nn.Parameter objects.
+Such lists are the data structure used by PyTorch to store the parameters of
+a neural network.
+"""
 
 
 import math
@@ -6,7 +10,14 @@ import torch
 import torch.nn
 
 
+EMPTY_PARAMETER_LIST = 'The parameter list is empty.'
+MISMATCHED_PARAMETER_LENGTH = 'The two parameters lists have mismatched lengths.'
+
+
 def l2_norm(vector) -> float:
+    # check for bad inputs
+    _check_inputs(vector)
+
     length = 0
 
     for p in vector:
@@ -16,6 +27,9 @@ def l2_norm(vector) -> float:
 
 
 def vector_addition(vector_a, vector_b) -> list:
+    # check for bad input
+    _check_inputs(vector_a, vector_b)
+
     new_vector = []
 
     for idx in range(len(vector_a)):
@@ -25,6 +39,9 @@ def vector_addition(vector_a, vector_b) -> list:
 
 
 def vector_subtraction(vector_a, vector_b) -> list:
+    # check for bad input
+    _check_inputs(vector_a, vector_b)
+
     new_vector = []
 
     for idx in range(len(vector_a)):
@@ -33,44 +50,68 @@ def vector_subtraction(vector_a, vector_b) -> list:
     return new_vector
 
 
-def scalar_multiplication(vector_a, scalar) -> list:
+def scalar_multiplication(vector, scalar) -> list:
+    # check for bad input
+    _check_inputs(vector)
+
     new_vector = []
 
-    for idx in range(len(vector_a)):
-        new_vector.append(vector_a[idx].clone().detach() * scalar)
+    for idx in range(len(vector)):
+        new_vector.append(vector[idx].clone().detach() * scalar)
 
     return new_vector
 
 
 def scalar_division(vector, scalar) -> list:
+    # check for bad input
+    _check_inputs(vector)
+
     return scalar_multiplication(vector, 1/scalar)
 
 
 def unit_vector(vector) -> list:
+    # check for bad input
+    _check_inputs(vector)
+
     return scalar_division(vector, l2_norm(vector))
 
 
 def vector_addition__(target_vector, vector):
+    # check for bad input
+    _check_inputs(target_vector, vector)
+
     for p_target, p in zip(target_vector, vector):
         p_target += p
 
 
 def vector_subtraction__(target_vector, vector):
+    # check for bad input
+    _check_inputs(target_vector, vector)
+
     for p_target, p in zip(target_vector, vector):
         p_target += p
 
 
 def scalar_multiplication__(target_vector, scalar):
+    # check for bad input
+    _check_inputs(target_vector)
+
     for p in target_vector:
         p *= scalar
 
 
 def scalar_division__(target_vector, scalar):
+    # check for bad input
+    _check_inputs(target_vector)
+
     for p in target_vector:
         p /= scalar
 
 
 def unit_vector__(target_vector):
+    # check for bad input
+    _check_inputs(target_vector)
+
     length = l2_norm(target_vector)
     for p in target_vector:
         p /= length
@@ -79,3 +120,14 @@ def unit_vector__(target_vector):
 def unrequire_grad__(model: torch.nn.Module):
     for p in model.parameters():
         p.requires_grad = False
+
+
+def _check_inputs(*args):
+    length = len(args[0])
+
+    if length == 0:
+        raise ValueError(EMPTY_PARAMETER_LIST)
+
+    for vector in args[1:]:
+        if len(vector) != length:
+            raise ValueError(MISMATCHED_PARAMETER_LENGTH)
