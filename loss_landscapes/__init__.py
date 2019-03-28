5b9498c54979269ce6623e1100023adbcca30b7c
marcellodebernardi <marcello1234@live.co.uk>
2019-03-28 19:17:49 +0000

    Add important methods to __init__.py

--- loss_landscapes/__init__.py
+++ loss_landscapes/__init__.py
@@ -1,1 +1,4 @@
-import loss_landscapes.backends as backends
+from loss_landscapes.compute import random_line
+from loss_landscapes.compute import random_plane
+from loss_landscapes.compute import linear_interpolation
+from loss_landscapes.compute import planar_interpolation
