aa5c6829acfce0ef0d806110d84bbe8706dbe693
marcellodebernardi <marcello1234@live.co.uk>
2019-05-09 19:49:04 +0100

    Refactor library with new ParameterVector

---
+++ loss_landscapes/advanced.py
@@ -0,0 +1,10 @@
+"""
+This module exposes functions for loss landscape operations which are more complex than simply
+computing the loss at different points in parameter space. This ranges from computing the
+principal curvature of the loss to things such as Kolsbjerg et al.'s Automated Nudged Elastic
+Band algorithm.
+"""
+
+
+
+
