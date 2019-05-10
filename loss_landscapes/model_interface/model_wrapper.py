3fdd09f3f4f4d4389b70a17ccec71e3cb48f2efd
marcellodebernardi <marcello1234@live.co.uk>
2019-05-10 19:05:17 +0100

    Refactor model_wrapper

--- loss_landscapes/model_interface/model_wrapper.py
+++ loss_landscapes/model_interface/model_wrapper.py
@@ -3,61 +3,20 @@ Defines functions for getting and setting the parameters of a model.
 """
 
 import copy
+import torch
+import torch.nn
 from loss_landscapes.model_interface.parameter_vector import ParameterVector
-
-SUPPORTED = {
-    'torch.nn.Module': 'torch',
-    'tensorflow.keras.Model': 'tf_keras'
-}
-
-
-def _import_torch():
-    import torch as t
-    return t
-
-
-def _import_dl_library(model_type):
-    if model_type == 'torch':
-        return _import_torch()
-
-
-def _detect_model_type(model) -> str:
-    """
-    Returns a string indicating the type and library of
-    :param model: the model of which we want to know the type
-    :return: a string indicating the deep learning library the model belongs to
-    """
-    model_type = str(type(model))
-    try:
-        return SUPPORTED[model_type]
-    except KeyError:
-        _raise_unrecognized_model_error(model_type)
-
-
-def _raise_unrecognized_model_error(model_type: str):
-    """
-    Raises an error informing the user that the deep learning library they are using is
-    not supported by loss_landscapes.
-    :return: none
-    """
-    # this is its own method because the raise expression below is used in multiple places
-    raise ValueError(
-        'Model type not recognized. Model is of type ' + str(model_type) + ', supported include ' + str(
-            SUPPORTED)
-    )
 
 
 class ModelWrapper:
     """ Wraps a model and defines the operations for getting its weights or updating them. """
     def __init__(self, model):
         super().__init__()
-        self.model = model
-        self.model_type = _detect_model_type(model)
-        self.library = _import_dl_library(self.model_type)
+        if not isinstance(model, torch.nn.Module):
+            raise ValueError('Model is not a subclass of torch.nn.Module.')
 
-        # torch-specific state
-        # torch_keys is used to maintain a consistent order of the internal numpy array lists
-        self.torch_keys = list(self.model.state_dict().keys()) if self.model_type == 'torch' else None
+        self.model = model
+        self.torch_keys = sorted(self.model.state_dict().keys())
 
     def get_model(self):
         """
@@ -71,20 +30,11 @@ class ModelWrapper:
         Returns the parameters of the model as a list of numpy arrays.
         :return: list of numpy arrays
         """
-        #
-        if self.model_type == 'torch':
-            parameters = []
-            # use keys from stored key list to ensure list is consistently ordered
-            for key in self.torch_keys:
-                parameters.append(copy.deepcopy(self.model.state_dict()[key].numpy()))
-            return ParameterVector(parameters)
-
-        elif self.model_type == 'tf_keras':
-            # tf and keras support coming in the future, but not yet implemented
-            _raise_unrecognized_model_error(self.model_type)
-
-        else:
-            _raise_unrecognized_model_error(self.model_type)
+        parameters = []
+        # use keys from stored key list to ensure list is consistently ordered
+        for key in self.torch_keys:
+            parameters.append(copy.deepcopy(self.model.state_dict()[key].numpy()))
+        return ParameterVector(parameters)
 
     def set_parameters(self, new_parameters: ParameterVector):
         """
@@ -92,8 +42,7 @@ class ModelWrapper:
         :param new_parameters: list of numpy arrays
         :return: none
         """
-        if self.model_type == 'torch':
-            new_state_dict = dict()
-            for i in range(len(new_parameters)):
-                new_state_dict[self.torch_keys[i]] = self.library.from_numpy(copy.deepcopy(new_parameters[i]))
-            self.model.load_dict(self.torch_keys)
+        new_state_dict = dict()
+        for i in range(len(new_parameters)):
+            new_state_dict[self.torch_keys[i]] = copy.deepcopy(new_parameters[i])
+        self.model.load_dict(new_state_dict)
