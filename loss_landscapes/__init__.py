ceca63b1c25948894ce31fd8dfeffdf736297f5e
marcellodebernardi <marcello.logins@outlook.com>
2019-06-05 07:20:25 +0100

    Import main.py methods in __init__.py

---
+++ loss_landscapes/__init__.py
@@ -0,0 +1,5 @@
+from loss_landscapes.main import point
+from loss_landscapes.main import linear_interpolation
+from loss_landscapes.main import random_line
+from loss_landscapes.main import planar_interpolation
+from loss_landscapes.main import random_plane
