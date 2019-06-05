21ee49b5ab07ede49f3811fdede74644f3bda835
marcellodebernardi <marcello.logins@outlook.com>
2019-06-05 08:34:25 +0100

    Fix broken imports

--- loss_landscapes/common/model_interface/torch/torch_vector.py
+++ loss_landscapes/common/model_interface/torch/torch_vector.py
@@ -1,4 +1,4 @@
-import common.model_interface.model_vector as model_vector
+import loss_landscapes.common.model_interface.model_vector as model_vector
 
 
 class TorchParameterVector(model_vector.ParameterVector):
