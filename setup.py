8e2b2a8bf516918a6a53d5785216f56c8ccc7fb6
marcellodebernardi <marcello1234@live.co.uk>
2019-08-30 11:39:30 +0100

    Bump version to 3.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name='loss_landscapes',
-    version='3.0.1',
+    version='3.0.2',
     packages=find_packages(exclude='tests'),
     url='https://github.com/marcellodebernardi/loss-landscapes',
     license='MIT',
